749ac13f787dd6b860edaeefe4f7d957043c749b
Praveen Kumar <gilboj@gmail.com>
2025-12-05 22:34:04 +0000

    modified

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { ABHAProvider } from './contexts/ABHAContext'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode> - Temporarily disabled for debugging
-    <ABHAProvider>
-      <App />
-    </ABHAProvider>
-  // </React.StrictMode>,
-)
+const Root = () => {
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: '2rem',
+      }}
+    >
+      Hello Macha 👋 – if you see this, React is working!
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);
